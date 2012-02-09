2f8ac4efa93450d9bf4c5ef98294ced3f3a10797
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2012-02-09 04:03:00 -0800

    using the new tar that doesnt need the directory to be created.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,25 +7,18 @@
 #
 
 pkg_url = "#{node[:automysqlbackup][:package_url]}/#{node[:automysqlbackup][:package_name]}.tar.gz"
-install_path = "#{node[:automysqlbackup][:install_location]}/#{node[:automysqlbackup][:package_name]}"
-pkg_local_path = "#{install_path}.tar.gz"
+pkg_local_path = "#{node[:automysqlbackup][:install_location]}/#{node[:automysqlbackup][:package_name]}"
+pkg_local_archive = "#{pkg_local_path}.tar.gz"
 
 package "tar"
 
-remote_file pkg_local_path do
+remote_file pkg_local_archive do
   source pkg_url
   mode "0755"
-  not_if {File.exists?(pkg_local_path)}
-end
-
-directory install_path do
-  owner "root"
-  group "root"
-  mode "0755"
-  action :create
+  not_if {File.exists?(pkg_local_archive)}
 end
 
 execute "install automysqlbackup" do
-  command "tar -xzvf #{pkg_local_path} -C #{install_path}"
-  not_if {File.directory?(install_path)}
+  command "tar -xzvf #{pkg_local_archive} -C #{node[:automysqlbackup][:install_location]}"
+  not_if {File.directory?(pkg_local_path)}
 end
